5faa2de3077cc78e0f0ea8f46d2adbc18bdb797d
vincent290587 <vincent.golle@gmail.com>
2021-08-24 15:50:15 +0200

    Updated JS

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const https = require('https')
 const options = {
   hostname: 'intervals.icu',
   port: 443,
-  path: '/api/v1/athlete/i30899/events?oldest=2021-08-23&newest=2021-08-24',
+  path: '/api/v1/athlete/i30899/events?oldest=2021-08-24&newest=2021-08-24',
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': 'Basic QVBJX0tFWTo1YW5ka2w2bW1xd3FodGFuZDF4a3dpemF2',
   },
+}
+
+const optionsW = {
+  hostname: 'intervals.icu',
+  port: 443,
+  path: '/api/v1/athlete/i30899/events/1399189/download.zwo',
+  method: 'GET',
+  headers: {
+    'Authorization': 'Basic QVBJX0tFWTo1YW5ka2w2bW1xd3FodGFuZDF4a3dpemF2',
+  },
+}
+
+// GET https://intervals.icu/api/v1/athlete/i30899/events/1399189/download.zwo
+
+callback2 = function(response) {
+  var str = ''
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  response.on('end', function () {
+    console.log(str);
+  });
 }
 
 callback = function(response) {
@@ -20,16 +43,17 @@ callback = function(response) {
     response.on('end', function () {
       console.log(str);
     });
+
+    const req2 = https.request(optionsW, callback2)
+
+    req2.on('error', error => {
+      console.error(error)
+    })
+
+    req2.end()
   }
 
-const req = https.request(options, res => {
-
-  console.log(`statusCode: ${res.statusCode}`)
-
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
+const req = https.request(options, callback)
 
 req.on('error', error => {
   console.error(error)
